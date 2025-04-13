6cbc9871e23ce8c0055b3cf85dca174ac1873e32
pawaruday06 <pawaruday06@gmail.com>
2025-04-13 17:59:31 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,21 @@
+import sys
+import logging
+
+def error_msg_details(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "error occur in python script name [{0}] line number [{1}] error msg[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+    
+    
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_msg_details(error_message,error_datail = error_datail)
+
+    def __str__(self):
+        return self.error_message
+
